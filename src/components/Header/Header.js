6a1672aac712d88ed5cca16f121eedffc548dff4
Thomas Kim <thomaskim@Thomass-Mac-mini-2.local>
2021-05-12 10:36:52 -0400

    Add homepage styling

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,35 +5,35 @@ import faceSpace from '../../images/facespace-logo.png'
 
 const authenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#create">Create Post</Nav.Link>
-    <Nav.Link href="#users">View Users</Nav.Link>
-    <Nav.Link href="#change-password">Change Password</Nav.Link>
-    <Nav.Link href="#sign-out">Sign Out</Nav.Link>
+    <Nav.Link className='link' href="#create">Create Post</Nav.Link>
+    <Nav.Link className='link' href="#users">View Users</Nav.Link>
+    <Nav.Link className='link' href="#change-password">Change Password</Nav.Link>
+    <Nav.Link className='link' href="#sign-out">Sign Out</Nav.Link>
   </Fragment>
 )
 
 const unauthenticatedOptions = (
   <Fragment>
-    <Nav.Link href="#sign-up">Sign Up</Nav.Link>
-    <Nav.Link href="#sign-in">Sign In</Nav.Link>
+    <Nav.Link className='link' href="#sign-up">Sign Up</Nav.Link>
+    <Nav.Link className='link' href="#sign-in">Sign In</Nav.Link>
   </Fragment>
 )
 
 const alwaysOptions = (
   <Fragment>
-    <Nav.Link href="#/">Home</Nav.Link>
+    <Nav.Link className='link' href="#/">Home</Nav.Link>
   </Fragment>
 )
 
 const Header = ({ user }) => (
   <Navbar bg="primary" variant="dark" expand="md">
-    <Navbar.Brand href="#" className="nav-heading">
-      <img src={faceSpace}/>
+    <Navbar.Brand href="#" className="nav-heading col-md-4">
+      <img className='img-responsive' src={faceSpace}/>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
+    <Navbar.Collapse id="basic-navbar-nav col-md-8">
       <Nav className="ml-auto">
-        { user && <span className="navbar-text mr-2">Welcome, {user.email}</span>}
+        { user && <span className="navbar-text link mr-2">Welcome, {user.email}</span>}
         { alwaysOptions }
         { user ? authenticatedOptions : unauthenticatedOptions }
       </Nav>
